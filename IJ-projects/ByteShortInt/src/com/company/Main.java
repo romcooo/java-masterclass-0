06fd6c478d0ee138640a55451be4f01700500f1c
roman.stubna <roman.stubna@embedit.cz>
2018-09-01 18:52:46 +0200

    added projects

---
+++ IJ-projects/ByteShortInt/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte firstByte = 1;
+	byte secondByte = 41;
+	byte newByte = (byte) (firstByte + secondByte);
+    }
+}
