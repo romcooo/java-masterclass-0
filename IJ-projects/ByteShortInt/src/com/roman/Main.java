66dd61619b156f680d584a7c0907eb39b864d603
roman.stubna <roman.stubna@embedit.cz>
2018-09-01 19:59:57 +0200

    progress

--- IJ-projects/ByteShortInt/src/com/roman/Main.java
+++ IJ-projects/ByteShortInt/src/com/roman/Main.java
@@ -1,11 +1,26 @@
 package com.roman;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	byte firstByte = 1;
 	byte secondByte = 41;
 	byte newByte = (byte) (firstByte + secondByte);
-	System.out.println(newByte);
+//	System.out.println(newByte);
+    //two ways of assigning a float
+    float float1 = 5.4f;
+    float float2 = (float) 5.4;
+    double double1 = 3d;
+
+    //get keyboard input for pounds, convert to kilos and print out
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("enter pounds:\n");
+    double pounds1 = keyboard.nextDouble();
+    double kilos1 = pounds1 * 0.453_592_37;
+    System.out.println(kilos1);
+
     }
 }
