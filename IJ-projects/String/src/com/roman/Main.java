9810e68b10927ce1f68a367f166caf87c80a8344
roman.stubna <roman.stubna@embedit.cz>
2018-09-23 20:12:09 +0200

    new projects in course 1

---
+++ IJ-projects/String/src/com/roman/Main.java
@@ -0,0 +1,16 @@
+package com.roman;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String string1 = "string1";
+        String string2 = "string2";
+        string2.concat("asd");
+//        string2 = string2.concat("asd");
+//        string1 = string1.concat("string1concat");
+        System.out.println("String1 = "+string1+" and string2 = "+string2);
+    }
+}
