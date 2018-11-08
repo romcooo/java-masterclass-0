073b64039734c6f8c87ff09698f7a8fd3909598a
romcooo <stubna@gmail.com>
2018-11-08 20:45:00 +0100

    overloading

--- IJ-projects/String/src/com/roman/Main.java
+++ IJ-projects/String/src/com/roman/Main.java
@@ -1,16 +1,36 @@
 package com.roman;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Main {
 
+    static String CM_VALIDATION_ERROR = "Validation error";
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if (feet < 0 || (0 > inches || inches > 12)) {
+            System.out.println(CM_VALIDATION_ERROR);
+            return -1;
+        }
+        double result = calcFeetAndInchesToCentimeters(feet*12 + inches);
+        System.out.println(feet+" feet, "+inches+" inches is equal to "+result + " cenitmeters.");
+        return result;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if (0 > inches) {
+            System.out.println(CM_VALIDATION_ERROR);
+            return -1;
+        }
+        double feet = inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches+" inches is equal to "+feet+" feet and "+remainingInches+" inches.");
+        return (inches*2.54);
+    }
+
     public static void main(String[] args) {
         // write your code here
-        String string1 = "string1";
-        String string2 = "string2";
-        string2.concat("asd");
-//        string2 = string2.concat("asd");
-//        string1 = string1.concat("string1concat");
-        System.out.println("String1 = "+string1+" and string2 = "+string2);
+        double inches = 2.5d;
+        double feet = 0d;
+        double result = calcFeetAndInchesToCentimeters(feet, inches);
+        System.out.println(result);
     }
 }
