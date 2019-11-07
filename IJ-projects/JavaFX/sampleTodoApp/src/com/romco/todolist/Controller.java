c5eed3889b8e07fbca52b41b77f525f6aa809fdd
romcooo.laptop <roman.stubna@embedit.cz>
2019-11-07 13:43:58 +0100

    start todo

--- IJ-projects/JavaFX/sampleTodoApp/src/com/romco/todolist/Controller.java
+++ IJ-projects/JavaFX/sampleTodoApp/src/com/romco/todolist/Controller.java
@@ -1,4 +1,24 @@
 package com.romco.todolist;
 
+import com.romco.todolist.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 public class Controller {
+
+    private List<TodoItem> todoItems;
+
+    public void initiaize() {
+        TodoItem item1 = new TodoItem("Mail birthday card",
+                "Buy a birthday card for John",
+                LocalDate.of(2019, Month.DECEMBER, 25));
+        TodoItem item2 = new TodoItem("Doctor's appointment",
+                "Visit Dr. House at 123 Hutch Boulevard.",
+                LocalDate.of(2019, Month.NOVEMBER, 21));
+        TodoItem item3 = new TodoItem("Pick up Doug at train station",
+                "  arriving on November 1st, at 11:00",
+                LocalDate.of(2019, Month.DECEMBER, 25));
+    }
 }
---
+++ IJ-projects/JavaFX/sampleTodoApp/src/com/romco/todolist/datamodel/TodoItem.java
@@ -0,0 +1,38 @@
+package com.romco.todolist.datamodel;
+
+import java.time.LocalDate;
+
+public class TodoItem {
+    private String description, details;
+    private LocalDate dueDate;
+
+    public TodoItem(String description, String details, LocalDate dueDate) {
+        this.description = description;
+        this.details = details;
+        this.dueDate = dueDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
