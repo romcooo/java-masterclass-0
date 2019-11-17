34539f59de293776d2df54cf2507acacf5a11b87
romcooo.desktop2 <stubna@gmail.com>
2019-11-17 23:15:02 +0100

    more thread

--- IJ-projects/chapter15/threads/src/com/romcooo/Main.java
+++ IJ-projects/chapter15/threads/src/com/romcooo/Main.java
@@ -1,9 +1,25 @@
 package com.romcooo;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello from the main thread!");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+
+        Thread anotherAnotherThread = new AnotherThread();
+        anotherAnotherThread.start();
+
+        new Thread() {
+            public void run() {
+                System.out.println("Hello from anon class");
+            }
+        }.start();
+
+        System.out.println("Hello again from the main thread.");
     }
 }
---
+++ IJ-projects/chapter15/threads/src/com/romcooo/ThreadColour.java
@@ -0,0 +1,5 @@
+package com.romcooo;
+
+public class ThreadColour {
+
+}
