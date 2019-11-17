a64b957484adb680283b98c6eabefdc4d59464a3
romcooo.desktop2 <stubna@gmail.com>
2019-11-17 22:36:45 +0100

    threads

---
+++ IJ-projects/chapter15/threads/src/com/romcooo/Main.java
@@ -0,0 +1,9 @@
+package com.romcooo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello from the main thread!");
+    }
+}
