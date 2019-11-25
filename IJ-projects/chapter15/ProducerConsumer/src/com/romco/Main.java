bf22a2a0f710abe5427e9636c8d6d783b6157366
romcooo.desktop2 <stubna@gmail.com>
2019-11-25 21:17:53 +0100

    asqwe

--- IJ-projects/chapter15/ProducerConsumer/src/com/romco/Main.java
+++ IJ-projects/chapter15/ProducerConsumer/src/com/romco/Main.java
@@ -3,6 +3,8 @@ package com.romco;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.romco.Main.EOF;
 
@@ -11,23 +13,47 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> buffer = new ArrayList<>();
-        MyProducer producer = new MyProducer(buffer, ThreadColour.ANSI_YELLOW);
-        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColour.ANSI_PURPLE);
-        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColour.ANSI_CYAN);
+        ReentrantLock bufferLock = new ReentrantLock();
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        MyProducer producer = new MyProducer(buffer, ThreadColour.ANSI_YELLOW, bufferLock);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColour.ANSI_PURPLE, bufferLock);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColour.ANSI_CYAN, bufferLock);
+
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColour.ANSI_WHITE + "I'm being printed for the Callable class");
+                return "This is the callable result";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (ExecutionException e) {
+            System.out.println("Something went wrong");
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted");
+        }
+
+        executorService.shutdown();
     }
 }
 
 class MyProducer implements Runnable {
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    public MyProducer(List<String> buffer, String color) {
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
 
     @Override
@@ -38,8 +64,12 @@ class MyProducer implements Runnable {
         for (String num: nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                synchronized (buffer) {
+
+                bufferLock.lock();
+                try {
                     buffer.add(num);
+                } finally {
+                    bufferLock.unlock();
                 }
 
                 Thread.sleep(random.nextInt(1000));
@@ -49,38 +79,62 @@ class MyProducer implements Runnable {
         }
 
         System.out.println(color + "Adding EOF and exiting.");
-        synchronized (buffer) {
+        bufferLock.lock();
+        try {
             buffer.add(EOF);
+        } finally {
+            bufferLock.unlock();
         }
+
     }
 }
 
 class MyConsumer implements Runnable {
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    public MyConsumer(List<String> buffer, String color) {
+    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
 
     @Override
     public void run() {
+        int counter = 0;
+
         while (true) {
-            synchronized (buffer) {
-                if (buffer.isEmpty()) {
-//                    try {
-//                        Thread.sleep(1);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-                    continue;
+//            bufferLock.lock();
+            if (bufferLock.tryLock()) {
+                try {
+                    if (buffer.isEmpty()) {
+                        //                    try {
+                        //                        Thread.sleep(1);
+                        //                    } catch (InterruptedException e) {
+                        //                        e.printStackTrace();
+                        //                    }
+                        //                    bufferLock.unlock();
+                        continue;
+                    }
+                    System.out.println(color + "Counter = " + counter);
+                    counter = 0;
+                    if (buffer.get(0).equals(EOF)) {
+                        System.out.println(color + "Exiting");
+                        //                    bufferLock.unlock();
+                        break;
+                    } else {
+                        System.out.println(color + "Removed " + buffer.remove(0));
+                    }
+                } finally {
+                    bufferLock.unlock();
                 }
-                if (buffer.get(0).equals(EOF)) {
-                    System.out.println(color + "Exiting");
-                    break;
-                } else {
-                    System.out.println(color + "Removed " + buffer.remove(0));
+            } else {
+                counter++;
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
