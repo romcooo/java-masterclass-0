9628c6de657e0d4c295e1410d50ed25b5a0726a4
romcooo.laptop <roman.stubna@embedit.cz>
2020-01-28 14:30:42 +0100

    spring

--- Spring-masterclass/tmp-dld-src/todolist/src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ Spring-masterclass/tmp-dld-src/todolist/src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -10,9 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -42,9 +40,21 @@ public class TodoItemController {
         return ViewNames.ITEMS_LIST;
     }
     
+    @GetMapping(Mappings.VIEW_ITEM)
+    public String viewItem(@RequestParam int id, Model model) {
+        log.info("Viewing item with id = {}", id);
+        TodoItem todoItem = todoItemService.getItem(id);
+        model.addAttribute(todoItem);
+        return ViewNames.VIEW_ITEM;
+    }
+    
     @GetMapping(Mappings.ADD_ITEM)
-    public String addEditItem(Model model) {
-        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id, Model model) {
+        log.info("editing id = {}", id);
+        TodoItem todoItem = todoItemService.getItem(id);
+        if (todoItem == null) {
+            todoItem = new TodoItem("", "", LocalDate.now());
+        }
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
     }
@@ -52,7 +62,18 @@ public class TodoItemController {
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
         log.info("todoItem from form = {}", todoItem);
-        todoItemService.addItem(todoItem);
+        if (todoItem.getId() == 0) {
+            todoItemService.addItem(todoItem);
+        } else {
+            todoItemService.updateItem(todoItem);
+        }
+        return "redirect:/" + Mappings.ITEMS;
+    }
+    
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id) {
+        todoItemService.removeItem(id);
+        log.info("Removed item with id={}", id);
         return "redirect:/" + Mappings.ITEMS;
     }
 }
--- Spring-masterclass/tmp-dld-src/todolist/src/main/webapp/WEB-INF/view/items_list.jsp
+++ Spring-masterclass/tmp-dld-src/todolist/src/main/webapp/WEB-INF/view/items_list.jsp
@@ -6,6 +6,8 @@
 </head>
 <body>
     <div align="center">
+    <c:url var="addUrl" value="${Mappings.ADD_ITEM}"/>
+    <a href="${addUrl}">New Item</a>
         <table border="1" cellpadding="5">
 
             <caption><h2>Todo Items</h2></caption>
@@ -13,13 +15,27 @@
             <tr>
                 <th>Title</th>
                 <th>Deadline</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
 
             <c:forEach var="item" items="${todoData.items}">
+                <c:url var="viewUrl" value="${Mappings.VIEW_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
+                <c:url var="editUrl" value="${Mappings.ADD_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
+                <c:url var="deleteUrl" value="${Mappings.DELETE_ITEM}">
+                    <c:param name="id" value="${item.id}"/>
+                </c:url>
+
                 <tr>
-                    <td><c:out value="${item.title}"/></td>
+                    <td><a href="${viewUrl}"><c:out value="${item.title}"/></a></td>
                     <td><c:out value="${item.deadline}"/></td>
-                <tr/>
+                    <td><a href="${editUrl}">Edit</a></td>
+                    <td><a href="${deleteUrl}">Delete</a></td>
+                </tr>
             </c:forEach>
         </table>
     </div>
