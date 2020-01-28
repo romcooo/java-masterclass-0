3efbaff2df1021483a1643a97f6c4fed95ce58e4
romcooo.laptop <roman.stubna@embedit.cz>
2020-01-28 16:15:59 +0100

    spring boot

---
+++ Spring-masterclass/Maven-projects/chapter7/practice-project/src/main/java/com/romco/config/MyWebApplicationInitializer.java
@@ -0,0 +1,34 @@
+package com.romco.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+@Slf4j
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+    
+    public static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+    
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("Starting up.");
+        
+        // create context
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+    
+        // create servlet
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+        
+        // register and configure the servlet
+        ServletRegistration.Dynamic registration = servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcherServlet);
+    
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
---
+++ Spring-masterclass/Maven-projects/chapter7/practice-project/src/main/java/com/romco/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.romco.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.romco")
+public class WebConfig {
+}
