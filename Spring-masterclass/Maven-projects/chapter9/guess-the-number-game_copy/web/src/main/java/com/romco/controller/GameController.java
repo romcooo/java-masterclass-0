de5448586e224a1cfee48af19efccfb018d522fa
romcooo.laptop <roman.stubna@embedit.cz>
2020-01-29 17:14:53 +0100

    some spring work

--- Spring-masterclass/Maven-projects/chapter9/guess-the-number-game_copy/web/src/main/java/com/romco/controller/GameController.java
+++ Spring-masterclass/Maven-projects/chapter9/guess-the-number-game_copy/web/src/main/java/com/romco/controller/GameController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -31,5 +33,24 @@ public class GameController {
         log.info("model = {}", model);
         // If the game is over, go to the game over page, else keep playing by going to the play page.
         return gameService.isGameOver() ? ViewNames.GAME_OVER : ViewNames.PLAY;
+    }
+    
+    // This corresponds to <form action="play" method="POST"> from play.html
+    @PostMapping(GameMappings.PLAY)
+    public String processMessage(@RequestParam int guess) {
+        log.info("Guess is: {}", guess);
+        gameService.checkGuess(guess);
+        return GameMappings.REDIRECT_PLAY;
+    }
+    
+    @GetMapping(GameMappings.RESTART)
+    public String restart() {
+        gameService.reset();
+        return GameMappings.REDIRECT_PLAY;
+    }
+    
+    @GetMapping(GameMappings.HOME)
+    public String home() {
+        return GameMappings.HOME;
     }
 }
