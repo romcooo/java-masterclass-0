cce062fb8b6b28e92fd09e7ade58b0711411a41d
romcooo.laptop <roman.stubna@embedit.cz>
2020-01-29 17:20:52 +0100

    fixed home

--- Spring-masterclass/Maven-projects/chapter9/guess-the-number-game_copy/web/src/main/java/com/romco/controller/GameController.java
+++ Spring-masterclass/Maven-projects/chapter9/guess-the-number-game_copy/web/src/main/java/com/romco/controller/GameController.java
@@ -49,8 +49,4 @@ public class GameController {
         return GameMappings.REDIRECT_PLAY;
     }
     
-    @GetMapping(GameMappings.HOME)
-    public String home() {
-        return GameMappings.HOME;
-    }
 }
