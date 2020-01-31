c50d52bda87223e355e329dbcabeec056c89e7ac
romcooo.desktop2 <stubna@gmail.com>
2020-01-31 21:11:04 +0100

    pewq

---
+++ Spring-masterclass/Maven-projects/chapter11/bb-bms/src/test/java/com/romco/bbbms/BbBmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romco.bbbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbBmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
